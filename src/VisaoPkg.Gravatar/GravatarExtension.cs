70e369b89a0dfaad3efd64fd3042b05607bcc0a6
lucas.cardoso <lucas.cardoso@visaogrupo.com.br>
2024-10-09 15:36:07 -0300

    upd

---
+++ src/VisaoPkg.Gravatar/GravatarExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace VisaoPkg.Gravatar;
+
+public static class GravatarExtension
+{
+    public static string ToGravatar(this string email)
+    {
+        if (string.IsNullOrEmpty(email))
+            return string.Empty;
+
+        using var md5 = MD5.Create();
+        var inputBytes = Encoding.ASCII.GetBytes(email);
+        var hashBytes = md5.ComputeHash(inputBytes);
+
+        var sb = new StringBuilder();
+        foreach (var item in hashBytes)
+            sb.Append(item.ToString("X2"));
+
+        return $"https://gravatar.com.br/avatar/{sb.ToString().ToLower()}";
+
+    }
+}
